65a6cfa7cfc2f146ff7991d572e4cc2619be0076
jgim <72037442+jgim@users.noreply.github.com>
2022-05-07 16:57:22 +0900

    Time: 66 ms (31.61%), Space: 14.4 MB (19.35%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -2,30 +2,27 @@ class Solution:
 	def search(self, nums: List[int], target: int) -> int:
 		def	binary_search(low, high):
 			mid = (low + high) // 2
+			print (nums[mid])
 			if nums[mid] == target:
 				return mid
-			elif nums[low] == target:
-				return low
-			elif nums[high] == target:
-				return high
 			if high - low <= 0:
 				return -1
-			if nums[low] < nums[mid]:
-				if nums[low] < target < nums[mid]:
+			if nums[low] <= nums[mid]:
+				if nums[low] <= target < nums[mid]:
 					return binary_search(low, mid - 1)
 				else:
 					return binary_search(mid + 1, high)
-			elif nums[mid] < nums[high]:
-				if nums[mid] < target < nums[high]:
+			elif nums[mid] <= nums[high]:
+				if nums[mid] < target <= nums[high]:
 					return binary_search(mid + 1, high)
 				else:
 					return binary_search(low, mid - 1)
 			return -1
 
-		lens = len(nums)
+		lens = len(nums) - 1
 		if not nums:
 			return -1
-		if nums[0] < nums[lens - 1]:
-			if nums[0] > target or nums[lens - 1] < target:
+		if nums[0] < nums[lens]:
+			if nums[0] > target or nums[lens] < target:
 				return -1
-		return binary_search(0, lens - 1)
+		return binary_search(0, lens)
