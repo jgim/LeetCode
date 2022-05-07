a3bd697f8293b4e19d77adc5abe5d5242574f11d
jgim <72037442+jgim@users.noreply.github.com>
2022-05-07 15:40:07 +0900

    Time: 65 ms (33.43%), Space: 14.4 MB (19.35%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,32 +1,24 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        def	binary_search(low, high):
-            mid = (low + high) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[low] == target:
-                return low
-            elif nums[high] == target:
-                return high
-            if high - low <= 0:
-                return -1
-            if nums[low] < nums[mid]:
-                if nums[low] < target < nums[mid]:
-                    return binary_search(low, mid - 1)
-                else:
-                    return binary_search(mid + 1, high)
-            elif nums[mid] < nums[high]:
-                if nums[mid] < target < nums[high]:
-                    return binary_search(mid + 1, high)
-                else:
-                    return binary_search(low, mid - 1)
-            else:
-                return -1
-
-        lens = len(nums)
-        if not nums:
-            return -1
-        if nums[0] < nums[lens - 1]:
-            if nums[0] > target or nums[lens - 1] < target:
-                return -1
-        return binary_search(0, lens - 1)
+	def search(self, nums: List[int], target: int) -> int:
+		lens = len(nums) - 1
+		if not nums:
+			return -1
+		if nums[0] < nums[lens]:
+			if nums[0] > target or nums[lens] < target:
+				return -1
+		low, high = 0, lens
+		while low <= high:
+			mid = (low + high) // 2
+			if nums[mid] == target:
+				return mid
+			if nums[low] <= nums[mid]:
+				if nums[low] <= target < nums[mid]:
+					high = mid - 1
+				else:
+					low = mid + 1
+			elif nums[mid] <= nums[high]:
+				if nums[mid] < target <= nums[high]:
+					low = mid + 1
+				else:
+					high = mid - 1
+		return -1
