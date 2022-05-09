fd046a3c99e8d59ad85112391d2861d11efb9499
jgim <72037442+jgim@users.noreply.github.com>
2022-05-10 01:00:44 +0900

    Time: 83 ms (5.07%), Space: 13.9 MB (25.60%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,40 +1,11 @@
 class Solution:
 	def isValid(self, s: str) -> bool:
-		def brackets(i, result, target):
-			lens = len(s)
-			while i < lens:
-				if s[i] == target:
-					return True, i + 1
-				elif s[i] == '{':
-					result, i = brackets(i + 1, result, '}')
-				elif s[i] == '(':
-					result, i = brackets(i + 1, result, ')')
-				elif s[i] == '[':
-					result, i = brackets(i + 1, result, ']')
-				elif s[i] == ' ':
-					i += 1
-				else:
-					return False, i
-				if result == False:
-					return False, i
-			return False, i
-
-		i = 0
-		lens = len(s)
-		if lens <= 1:
+		stack, dictionary = [],{'{':'}', '[':']', '(':')'}
+		for i in s:
+			if i in dictionary:
+				stack.append(i)
+			elif len(stack) == 0 or i != dictionary[stack.pop()]:
+				return False
+		if len(stack) != 0:
 			return False
-		result = True
-		while i < lens:
-			if s[i] == '{':
-				result, i = brackets(i + 1, result, '}')
-			elif s[i] == '[':
-				result, i = brackets(i + 1, result, ']')
-			elif s[i] == '(':
-				result, i = brackets(i + 1, result, ')')
-			elif s[i] == ' ':
-				i += 1
-			else:
-				return False
-			if result == False:
-				return False
 		return True
