a7b726fee90e6166661d24c48a6318c1f53ab948
jgim <72037442+jgim@users.noreply.github.com>
2022-05-09 23:47:58 +0900

    Time: 46 ms (43.39%), Space: 17.7 MB (25.60%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,54 +1,16 @@
 class Solution:
 	def isValid(self, s: str) -> bool:
-		def curlyBrackets(i, result):
+		def brackets(i, result, target):
 			lens = len(s)
 			while i < lens:
-				if s[i] == '}':
+				if s[i] == target:
 					return True, i + 1
 				elif s[i] == '{':
-					result, i = curlyBrackets(i + 1, result)
+					result, i = brackets(i + 1, result, '}')
 				elif s[i] == '(':
-					result, i = roundBrackets(i + 1, result)
+					result, i = brackets(i + 1, result, ')')
 				elif s[i] == '[':
-					result, i = squareBrackets(i + 1, result)
-				elif s[i] == ' ':
-					i += 1
-				else:
-					return False, i
-				if result == False:
-					return False, i
-			return False, i
-
-		def squareBrackets(i, result):
-			lens = len(s)
-			while i < lens:
-				if s[i] == ']':
-					return True, i + 1
-				elif s[i] == '{':
-					result, i = curlyBrackets(i + 1, result)
-				elif s[i] == '(':
-					result, i = roundBrackets(i + 1, result)
-				elif s[i] == '[':
-					result, i = squareBrackets(i + 1, result)
-				elif s[i] == ' ':
-					i += 1
-				else:
-					return False, i
-				if result == False:
-					return False, i
-			return False, i
-
-		def roundBrackets(i, result):
-			lens = len(s)
-			while i < lens:
-				if s[i] == ')':
-					return True, i + 1
-				elif s[i] == '{':
-					result, i = curlyBrackets(i + 1, result)
-				elif s[i] == '(':
-					result, i = roundBrackets(i + 1, result)
-				elif s[i] == '[':
-					result, i = squareBrackets(i + 1, result)
+					result, i = brackets(i + 1, result, ']')
 				elif s[i] == ' ':
 					i += 1
 				else:
@@ -63,13 +25,12 @@ class Solution:
 			return False
 		result = True
 		while i < lens:
-			print (i)
 			if s[i] == '{':
-				result, i = curlyBrackets(i + 1, result)
+				result, i = brackets(i + 1, result, '}')
 			elif s[i] == '[':
-				result, i = squareBrackets(i + 1, result)
+				result, i = brackets(i + 1, result, ']')
 			elif s[i] == '(':
-				result, i = roundBrackets(i + 1, result)
+				result, i = brackets(i + 1, result, ')')
 			elif s[i] == ' ':
 				i += 1
 			else:
