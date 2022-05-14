8cc5d780c0b17ad6106c59d14ab10873a58023a2
jgim <72037442+jgim@users.noreply.github.com>
2022-05-14 16:39:54 +0900

    Time: 73 ms (41.73%), Space: 15.2 MB (11.95%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,12 +1,3 @@
 class Solution:
 	def isAnagram(self, s: str, t: str) -> bool:
-		dictionary1, dictionary2 = {}, {}
-		for i in s:
-			if i not in dictionary1:
-				dictionary1[i] = 1
-			dictionary1[i] += 1
-		for i in t:
-			if i not in dictionary2:
-				dictionary2[i] = 1
-			dictionary2[i] += 1
-		return dictionary1 == dictionary2
+		return sorted(s) == sorted(t)
